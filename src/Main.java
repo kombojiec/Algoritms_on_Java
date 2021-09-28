0e84125b163c4c2bad3cf6c52b63191a4947946b
Kulakov Pavel <kombojiec@gmail.com>
2021-09-28 08:46:27 +0300

    Create Main class

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int data[] = {99, 7, 54, 6, 3, 7,1, 85, 16};
+        Sort.insertionSort(data);
+        System.out.println(Arrays.toString(data));
+
+    }
+}
